57f9c2f7b7b2cbcd9d1d60454b310d26c948fe8a
Ameer Al Issa <ameerwisam97@gmail.com>
2022-11-07 08:06:57 +0300

    add key form

--- Forms/KeyForm.Designer.cs
+++ Forms/KeyForm.Designer.cs
@@ -29,58 +29,37 @@ namespace IssaForms.Forms
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
             this.SuspendLayout();
             // 
-            // button1
+            // tableLayoutPanel1
             // 
-            this.button1.Location = new System.Drawing.Point(12, 12);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(96, 63);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(12, 81);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(96, 63);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
-            // 
-            // button3
-            // 
-            this.button3.Location = new System.Drawing.Point(12, 150);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(96, 63);
-            this.button3.TabIndex = 2;
-            this.button3.Text = "button3";
-            this.button3.UseVisualStyleBackColor = true;
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(12, 12);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(552, 502);
+            this.tableLayoutPanel1.TabIndex = 3;
             // 
             // KeyForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(120, 223);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(576, 526);
+            this.Controls.Add(this.tableLayoutPanel1);
             this.Name = "KeyForm";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.KeyForm_Load);
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
     }
 }
--- Forms/KeyForm.cs
+++ Forms/KeyForm.cs
@@ -12,34 +12,102 @@ namespace IssaForms.Forms
 {
     public partial class KeyForm : Form
     {
-        private TextBox _colonForm;
-        private string data = "33 ";
+        private TextBox _selectedTextBox;
+
+        private List<string> ColonsShoutCuts = new List<string>
+        {
+            "lower esophageal erosions (GERD LA class A) ",
+            "normal ",
+            "Patent lumen ",
+            "Lax cardia (Hill grade 1) ",
+            "Erythematous antral mucosa ",
+            "Nodular antral mucosa  ",
+            "Anterior wall ucler ",
+            "Posterior wall ucler ",
+            "Granular erythematous antral mucosa",
+            "Prolapse pastropathy ",
+            "normal gastric folds ",
+            "Dyspepsia ",
+            "Reflux symptoms ",
+            "Anemia ",
+            "Weight loss ",
+            "H.pylori gastritis ? ",
+            "multiple biopsies taken.",
+        };
 
         public KeyForm(TextBox colonForm)
         {
-            _colonForm = colonForm;
+            _selectedTextBox = colonForm;
             InitializeComponent();
+        }
 
-            foreach (var item in Controls.OfType<Button>())
+        //private void CloseForm(object sender, EventArgs e)
+        //{
+            
+        //}
+
+        //private void button1_Click(object sender, EventArgs e)
+        //{
+        //    data = "Hi a";
+        //}
+
+        //private void button2_Click(object sender, EventArgs e)
+        //{
+
+        //}
+
+
+
+
+
+
+        void b_Click(object sender, EventArgs e)
+        {
+            var b = sender as Button;
+            if (b != null)
             {
-                item.Click += CloseForm;
+                _selectedTextBox.Text += b.Text;
+                _selectedTextBox.Select(_selectedTextBox.Text.Length, 0);
+                Close();
             }
+            //MessageBox.Show(string.Format("{0} Clicked", b.Text));
         }
 
-        private void CloseForm(object sender, EventArgs e)
+        private void KeyForm_Load(object sender, EventArgs e)
         {
-            _colonForm.Text += data;
-            Close();
-        }
+            var rowCount = ColonsShoutCuts.Count;
+            //var columnCount = 4;
 
-        private void button1_Click(object sender, EventArgs e)
-        {
-            data = "Hi a";
-        }
+            //this.tableLayoutPanel1.ColumnCount = columnCount;
+            this.tableLayoutPanel1.RowCount = rowCount;
 
-        private void button2_Click(object sender, EventArgs e)
-        {
+            this.tableLayoutPanel1.ColumnStyles.Clear();
+            this.tableLayoutPanel1.RowStyles.Clear();
 
+            //for (int i = 0; i < columnCount; i++)
+            //{
+            //    this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100 / columnCount));
+            //}
+            for (int i = 0; i < rowCount; i++)
+            {
+                this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100 / rowCount));
+            }
+
+            for (int i = 0; i < rowCount; i++)
+            {
+                var b = new Button();
+                b.Text = ColonsShoutCuts[i];
+                b.Name = string.Format("b_{0}", i + 1);
+                b.Click += b_Click;
+                b.Dock = DockStyle.Fill;
+                this.tableLayoutPanel1.Controls.Add(b);
+            }
+
+
+            //foreach (var item in Controls.OfType<Button>())
+            //{
+            //    item.Click += CloseForm;
+            //}
         }
     }
 }
